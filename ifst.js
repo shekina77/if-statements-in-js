609c1000d712e7c80a78bd3b20091f5b5018f2d7
unknown <shekinadhanunjay@gmail.com>
2024-10-22 14:18:42 +0530

    statements in js

---
+++ ifst.js
@@ -0,0 +1,33 @@
+var age=22;
+if (age > 18){
+    console.log("eligible for vote")
+}else{
+
+    console.log("not eligible for vote")
+}
+
+var rainy=true;
+if(rainy){
+    console.log("its pouring outside")
+}else{
+    console.log("not raining")
+}
+ 
+var marks=76
+if(marks > 90){
+    console.log("A+")
+}else if(marks> 81 && marks<= 90){
+    console.log("A")
+
+}else if(marks> 71 && marks<= 80){
+    console.log("B+")
+}else if( marks> 61 &&marks<= 70){
+    console.log("C+")
+}else if( marks> 51 && marks<= 60){
+    console.log("C")
+}else if( marks> 40 &&marks<= 50){
+    console.log("D")
+}else{
+    console.log("fail")
+}
+
